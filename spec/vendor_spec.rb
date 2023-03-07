27d430859c81714586d7090e3f068cb222dcd018
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-03-07 09:11:56 -0700

    Test: create ininitialize testing for vendor class

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -3,5 +3,17 @@ require './lib/vendor'
 
 RSpec.describe Vendor do
   
-  
+  before(:each) do
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  describe 'initialize' do
+    it 'exists and has attributes' do
+      expect(@vendor).to be_a(Vendor)
+      expect(@vendor.name).to eq("Rocky Mountain Fresh")
+      expect(@vendor.inventory).to eq({})
+    end
+  end
 end
