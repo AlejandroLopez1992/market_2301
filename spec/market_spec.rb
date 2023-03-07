c32ec7890ae85e8dc511720b6215ae5380d0d7d4
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-03-07 10:33:01 -0700

    Test: create testing for total_inventory method

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -61,4 +61,36 @@ RSpec.describe Market do
       expect(@market.sorted_item_list).to eq(["Banana Nice Cream", 'Peach', "Peach-Raspberry Nice Cream", 'Tomato'])
     end
   end
+
+  describe 'total_inventory' do
+    it 'market shows total items sold in by quantity and vendors selling item' do
+      expected = {
+        @item1 => {
+          quantity: 100,
+          vendors: [@vendor1, @vendor3]
+        },
+        @item2 => {
+          quantity: 7,
+          vendors: [@vendor1]
+        },
+        @item3 => {
+          quantity: 25,
+          vendors: [@vendor2]
+        },
+        @item4 => {
+          quantity: 50,
+          vendors: [@vendor2]
+        }
+      }
+      @vendor1.stock(@item1, 35)  
+      @vendor1.stock(@item2, 7) 
+      @vendor2.stock(@item4, 50)   
+      @vendor2.stock(@item3, 25)
+      @vendor3.stock(@item1, 65) 
+      @market.add_vendor(@vendor1) 
+      @market.add_vendor(@vendor2) 
+      @market.add_vendor(@vendor3)
+      expect(@market.total_inventory).to eq(expected)
+    end
+  end
 end
