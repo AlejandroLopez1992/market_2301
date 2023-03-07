7bc93104ecf6d4ce931862324747d3e77edfcd4f
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-03-07 09:29:38 -0700

    Test: add testing for multiple added items and summed quantities

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -28,8 +28,13 @@ RSpec.describe Vendor do
     it 'multiple items can be stocked in inventory and quantity is added' do
       @vendor.stock(@item1, 30)
       expect(@vendor.check_stock(@item1)).to eq(30)
-      
-
+      @vendor.stock(@item1, 25)
+      expect(@vendor.check_stock(@item1)).to eq(55)
+      @vendor.stock(@item2, 12)
+      expect(@vendor.inventory).to eq({
+        @item1 => 55
+        @item2 => 12
+      })
     end
   end
 end
