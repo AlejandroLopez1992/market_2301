5073e937aaa0c586c220c30d2f65e0668e44f178
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-03-07 10:14:47 -0700

    Test: add testing for sorted_item_list method

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -47,4 +47,18 @@ RSpec.describe Market do
       expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
     end
   end
+
+  describe 'sorted_item_list' do
+    it 'create a list of alphabetically sorted items in stock' do
+      @vendor1.stock(@item1, 35)  
+      @vendor1.stock(@item2, 7) 
+      @vendor2.stock(@item4, 50)   
+      @vendor2.stock(@item3, 25)
+      @vendor3.stock(@item1, 65) 
+      @market.add_vendor(@vendor1) 
+      @market.add_vendor(@vendor2) 
+      @market.add_vendor(@vendor3)
+      expect(@market.sorted_item_list).to eq(["Banana Nice Cream", 'Peach', "Peach-Raspberry Nice Cream", 'Tomato'])
+    end
+  end
 end
