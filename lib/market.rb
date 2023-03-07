e5d67d9ced99443069b3ec32c903af09aea02ff6
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-03-07 12:00:13 -0700

    Final commit

--- lib/market.rb
+++ lib/market.rb
@@ -26,4 +26,19 @@ class Market
       end
     end.flatten.uniq.sort { |a, b| a <=> b }
   end
+
+  def total_inventory
+    items = Hash.new {|item, details| item[details] = {} }
+    item_details = Hash.new(0)
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        items[item] = item_details
+          item_details[:quantity] += vendor.check_stock(item) if item == vendor.inventory.first
+          require 'pry'; binding.pry
+          item_details[:vendors] = vendors_that_sell(item) if vendor.inventory.include?(item) 
+      end
+    
+    end
+    items
+  end
 end
